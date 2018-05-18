f4f7a4f38ceac38fae8a0aeefd57d6db80982f86
NimbusScale <joe@nimbusscale.com>
2018-05-17 19:46:20 -0700

    exception messages

--- c7n_notifiers/notifiers/slack_notifier.py
+++ c7n_notifiers/notifiers/slack_notifier.py
@@ -5,6 +5,7 @@ from operator import itemgetter
 import os
 import logging
 import string
+import traceback
 import urllib.request
 
 import jinja2
@@ -20,7 +21,7 @@ logger.setLevel(logging.DEBUG)
 resource_mappings = lib.resources.get_mappings()
 
 
-def send_slack_message(webhook_url, message):
+def send_slack_message(webhook_url, message, color='#6d6c6c'):
     if type(message) is not str:
         raise TypeError(
             "Slack message must be string, but is {}".format(type(message))
@@ -34,10 +35,10 @@ def send_slack_message(webhook_url, message):
     message_body = {
         'attachments': [
             {
-                'color': 'warning',
+                'color': color,
                 'footer': footer_text,
                 'text': message
-             }
+            }
         ]
     }
 
@@ -48,6 +49,31 @@ def send_slack_message(webhook_url, message):
                                  data=post_data)
     response = urllib.request.urlopen(req)
     logger.debug("Message response: {}".format(response))
+
+
+def format_exception_message(c7n_message, exception):
+    tb = ''.join(traceback.format_exception(
+        etype=type(exception),
+        value=exception,
+        tb=exception.__traceback__)
+    )
+
+    # format JSON to something better readable in the slack message
+    formatted_c7n_message = json.dumps(c7n_message, indent=4)
+
+    slack_message_info = {
+        'traceback': tb,
+        'c7n_message': formatted_c7n_message
+    }
+
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    template_path = current_dir + "/templates"
+    jinja_env = jinja2.Environment(
+        loader=jinja2.FileSystemLoader(template_path)
+    )
+    slack_message_template = jinja_env.get_template('exception')
+    slack_message = slack_message_template.render(**slack_message_info)
+    return slack_message
 
 
 def lambda_handler(event, context):
@@ -71,113 +97,127 @@ def lambda_handler(event, context):
     resources_data = c7n_message['resources']
     region = c7n_message['region']
 
-    resources = []
-    for resource_info in resources_data:
-        resource_info = lib.resources.get_resource_info(
-            resource_type,
-            resource_info,
-            resource_mappings=resource_mappings[resource_type]
+    # Try to get the data from the c7n_message and format a message for
+    # slack If an exception is encountered, send the error to slack and
+    # re-raise the exception
+    try:
+        resources = []
+        for resource_info in resources_data:
+            resource_info = lib.resources.get_resource_info(
+                resource_type,
+                resource_info,
+                resource_mappings=resource_mappings[resource_type]
+            )
+            # Adding region to resource_info makes rendering the resource link
+            # easier
+            resource_info['region'] = region
+            resources.append(resource_info)
+
+        logger.debug("resources: {}".format(resources))
+
+        # Sort resources by CreationDateTime
+        resources.sort(key=itemgetter('creation_datetime'), reverse=True)
+
+        # Formatting resource info in Python since slack doesn't support robust
+        # formatting.
+        formatted_lines = []
+        resource_id_pad = 22
+        resource_name_pad = 15
+        creation_dt_pad = 19
+        creator_pad = 12
+        line_layout = (
+            "{:<{resource_id_pad}}  {:<{resource_name_pad}}  "
+            "{:<{creation_dt_pad}}  {:<{creator_pad}}"
         )
-        # Adding region to resource_info makes rendering the resource link
-        # easier
-        resource_info['region'] = region
-        resources.append(resource_info)
-
-    logger.debug("resources: {}".format(resources))
-
-    # Sort resources by CreationDateTime
-    resources.sort(key=itemgetter('creation_datetime'), reverse=True)
-
-    # Formatting resource info in Python since slack doesn't support robust
-    # formatting.
-    formatted_lines = []
-    resource_id_pad = 22
-    resource_name_pad = 15
-    creation_dt_pad = 19
-    creator_pad = 12
-    line_layout = (
-        "{:<{resource_id_pad}}  {:<{resource_name_pad}}  "
-        "{:<{creation_dt_pad}}  {:<{creator_pad}}"
-    )
-    header_line = line_layout.format("ResourceId",
-                                     "ResourceName",
-                                     "CreationDateTime",
-                                     "Creator",
-                                     resource_id_pad=resource_id_pad,
-                                     resource_name_pad=resource_name_pad,
-                                     creation_dt_pad=creation_dt_pad,
-                                     creator_pad=creator_pad
-                                     )
-    formatted_lines.append(header_line)
-    for resource_info in resources:
-        # If Name is a tag and is not set then JMESpath returns an empty list
-        # in this case set the Name to empty
-        if type(resource_info['name']) is list:
-            if len(resource_info['name']) == 0:
-                name = ""
+        header_line = line_layout.format("ResourceId",
+                                         "ResourceName",
+                                         "CreationDateTime",
+                                         "Creator",
+                                         resource_id_pad=resource_id_pad,
+                                         resource_name_pad=resource_name_pad,
+                                         creation_dt_pad=creation_dt_pad,
+                                         creator_pad=creator_pad
+                                         )
+        formatted_lines.append(header_line)
+        for resource_info in resources:
+            # If Name is a tag and is not set then JMESpath returns an empty list
+            # in this case set the Name to empty
+            if type(resource_info['name']) is list:
+                if len(resource_info['name']) == 0:
+                    name = ""
+                else:
+                    name = resource_info['name'][0][:resource_name_pad]
             else:
-                name = resource_info['name'][0][:resource_name_pad]
-        else:
-            name = resource_info['name'][:resource_name_pad]
+                name = resource_info['name'][:resource_name_pad]
 
-        # If Creator tag is not set then JMESpath returns an empty list
-        # in this case set the creator to empty
-        if len(resource_info['creator']) == 0:
-            creator = ""
-        else:
-            creator = resource_info['creator'][0][:creator_pad]
+            # If Creator tag is not set then JMESpath returns an empty list
+            # in this case set the creator to empty
+            if len(resource_info['creator']) == 0:
+                creator = ""
+            else:
+                creator = resource_info['creator'][0][:creator_pad]
 
-        resource = resource_info['id'][:resource_id_pad]
-        # Padding needs to be calculated for each resource id as slack renders
-        # the link, which removes many characters on screen,
-        # so need to add white space to compensate the removal of characters
-        # when rendered.
-        resource_pad = resource_id_pad
-        if resource_info.get('url'):
-            resource_url = string.Template(
-                resource_info['url']
-            ).substitute(resource_info)
-            resource_link = '<{}|{}>'.format(resource_url, resource)
-            resource_pad = (
-                len(resource_link) - len(resource) + resource_id_pad
+            resource = resource_info['id'][:resource_id_pad]
+            # Padding needs to be calculated for each resource id as slack renders
+            # the link, which removes many characters on screen,
+            # so need to add white space to compensate the removal of characters
+            # when rendered.
+            resource_pad = resource_id_pad
+            if resource_info.get('url'):
+                resource_url = string.Template(
+                    resource_info['url']
+                ).substitute(resource_info)
+                resource_link = '<{}|{}>'.format(resource_url, resource)
+                resource_pad = (
+                    len(resource_link) - len(resource) + resource_id_pad
+                )
+                resource = resource_link
+
+            datetime_string = resource_info['creation_datetime'].strftime(
+                '%Y-%m-%d %H:%M:%S'
             )
-            resource = resource_link
 
-        datetime_string = resource_info['creation_datetime'].strftime(
-            '%Y-%m-%d %H:%M:%S'
-        )
-
-        formatted_lines.append(
-            line_layout.format(resource,
-                               name,
-                               datetime_string,
-                               creator,
-                               resource_id_pad=resource_pad,
-                               resource_name_pad=resource_name_pad,
-                               creation_dt_pad=creation_dt_pad,
-                               creator_pad=creator_pad
+            formatted_lines.append(
+                line_layout.format(resource,
+                                   name,
+                                   datetime_string,
+                                   creator,
+                                   resource_id_pad=resource_pad,
+                                   resource_name_pad=resource_name_pad,
+                                   creation_dt_pad=creation_dt_pad,
+                                   creator_pad=creator_pad
+                                   )
             )
+
+        slack_message_info = {}
+        if c7n_message['account'] != '':
+            slack_message_info['account_info'] = "{} ({})".format(
+                c7n_message['account_id'],
+                c7n_message['account']
+            )
+        else:
+            slack_message_info['account_info'] = c7n_message['account_id']
+
+        slack_message_info['region'] = region
+        slack_message_info['resource_type'] = resource_type
+        slack_message_info['resources'] = "\n".join(formatted_lines)
+
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        template_path = current_dir + "/templates"
+        jinja_env = jinja2.Environment(
+            loader=jinja2.FileSystemLoader(template_path)
         )
+        slack_message_template = jinja_env.get_template(message_template)
+        slack_message = slack_message_template.render(**slack_message_info)
+        send_slack_message(webhook_url, slack_message)
+    # Yes this is broad but we want to send info on any exception to slack
+    except Exception as e:
+        slack_message = format_exception_message(c7n_message, e)
+        send_slack_message(webhook_url, slack_message, color='#0f0f0f')
+        raise
 
-    slack_message_info = {}
-    if c7n_message['account'] != '':
-        slack_message_info['account_info'] = "{} ({})".format(
-            c7n_message['account_id'],
-            c7n_message['account']
-        )
-    else:
-        slack_message_info['account_info'] = c7n_message['account_id']
 
-    slack_message_info['region'] = region
-    slack_message_info['resource_type'] = resource_type
-    slack_message_info['resources'] = "\n".join(formatted_lines)
 
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    template_path = current_dir + "/templates"
-    jinja_env = jinja2.Environment(
-        loader=jinja2.FileSystemLoader(template_path)
-    )
-    slack_message_template = jinja_env.get_template(message_template)
-    slack_message = slack_message_template.render(**slack_message_info)
 
-    send_slack_message(webhook_url, slack_message)
+
+
